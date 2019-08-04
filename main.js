a6adfbfe83c2f8232d2fe4e61c6bf0c65ffc816a
Marek Poláček <marpolda@gmail.com>
2019-08-04 22:49:12 +0200

    Swearing words update
    Another constructor

--- main.js
+++ main.js
@@ -6,13 +6,15 @@
  * File: main.js
  *****************************************/
 
+// Required libraries
 require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client({disableEveryone: true});
 const mysql = require("mysql");
 const fs = require("fs");
 
-client.credentials = {      // 
+// Setup credentials
+client.credentials = {
     token: process.env.token,
     mysql: {
         host: process.env.mysql_host,
@@ -23,31 +25,24 @@ client.credentials = {      //
     }
 };
 
+// Load default settings
 client.default_settings = Object.freeze({
     prefix: "$$",
-    diceroll_emoji_default: true,
     diceroll_emoji_base: "\uFE0F\u20E3",        // Dice Roll command emoji base of keypads (used as "#\uFE0F\u20E3"
                                                 // where # is a digit from 1 to 6 = 6 sides of a dice)
-    coinflip_emoji_default: true,
     coinflip_emoji_base: Object.freeze({
         heads: "\uD83D\uDC78",    // Princess Emoji
         tails: "\uD83E\uDD85"     // Eagle Emoji
     }),
-    votepoll_emoji_default: true,
     votepoll_emoji_base: Object.freeze({
         vote_yes: "\uD83D\uDC4D",
         vote_no: "\uD83D\uDC4E"
-    }),
-    roles_set: Object.freeze({
-        owner: false,
-        admin: false,
-        moderator: false,
-        developer: false
     })
 });
 
+// Current settings as previously saved in MySQL database
 client.settings = {
-    guilds: {},
+    guilds: {},     // Guild dependent settings filled programmatically (mapped by their ids) -> see templates
     developers: {
         chief: "370298001597399044",
         additional: []
@@ -56,11 +51,12 @@ client.settings = {
 // Current settings => it needs to be filled by the mysql settings => settings per guild
 // Currently not used
 
+// Bans and mutes settings
 client.bans = {};
 client.mutes = {};
 
-client.current_settings = {     // Subject to change, actually
-    prefix: process.env.default_prefix,
+client.current_settings = {     // deprecated => gonna be deleted, refactored and replaced by mysql settings
+    prefix: "$$",
     diceroll_emoji_default: true,       // will be false if set to custom
     diceroll_emoji_base: "\uFE0F\u20E3",    // will be list of 6 emoji ids if set to custom
     coinflip_emoji_default: true,       // will be false if set to custom
@@ -82,30 +78,12 @@ client.current_settings = {     // Subject to change, actually
 };
 
 // PERFORM A MYSQL CONNECTION == TODO
-// const mysql_connection = mysql.createConnection({
-//     host     : client.credentials.mysql.host,
-//     user     : client.credentials.mysql.user,
-//     password : client.credentials.mysql.pswd,
-//     database : client.credentials.mysql.db
-// });
 
-// mysql_connection.connect();
-// mysql_connection.query('SELECT * FROM config_misc', (err, results, fields) => {
-//     if(err) return mysql_connection.query('CREATE TABLE config_misc (prefix VARCHAR(20) PRIMARY KEY);', (err, results, fields) => {
-//         if(err) throw(err);
-//         console.log('Created table "config_misc"...');
-//         mysql_connection.query(`INSERT INTO config_misc (prefix) VALUES (${client.default_settings.prefix})`, (err, results, fields) => {
-//             if(err) throw(err);
-//             console.log(`Successfully inserted "${client.default_settings.prefix}" into 'prefix'.`);
-//         });
-//     });
-    
-//     // TODO
-// });
-
+// Create a collection of bot commands
 client.commands_collection = new Discord.Collection();  // Save commands here
 
-fs.readdir("./commands/", (err, files) => {     // Load different commands with this primitive command handler
+// Load different commands with this primitive command handler
+fs.readdir("./commands/", (err, files) => {
     console.log("Loading and registering command files...");
     if(err) console.log(err);
 
@@ -122,12 +100,16 @@ fs.readdir("./commands/", (err, files) => {     // Load different commands with 
     console.log("Done...");
 });
 
+// This will be run when the bot is ready to roll out
 client.on('ready', () => {
     console.log(`Logged in as "${client.user.tag}" with id "${client.user.id}".`);  // Says when the bot is ready
 });
 
+// Scan a message for triggers
 client.on('message', message => {
     if (message.channel.type === "dm" || message.channel.type === "group") return; // ignore direct messages or group dm messages
+
+    // TODO: Swearing guard
     
     let prefix = client.current_settings.prefix;
     let msgArray = message.content.split(" ");
