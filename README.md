e8ab555fb60e576180ff86d39e92973ade5c3850
Marek Poláček <marpolda@gmail.com>
2019-08-05 17:59:32 +0200

    New development
    MySQL init

--- README.md
+++ README.md
@@ -1,4 +1,73 @@
-# Joker-Discord-Chat-Bot
+# Joker Discord Chat Bot
 Discord chat bot for automated messages guard, users management and entertainment 
 
-This project is currently under construction and heavy development. Details will be released soon.
+_This project is currently under construction._
+
+## Sneak preview
+Please note that the first version of the bot is still in development, so many things may change.
+
+Bot is currently in early development and testing phase. This early release is not yet out,
+but here's the sneak preview of what you can expect from the bot's features. It is a short list
+and may change through time being as I decide for more features, features change or features removal.
+
+If you want to add the bot before it's released, click following link to add the bot to your server:  
+https://discordapp.com/oauth2/authorize?client_id=484988029841440808&scope=bot&permissions=8
+
+  * Server dependent settings => Each server the bot is in can hold various settings, thanks
+    to use of JSON object and MySQL database for settings. Each ban and mute is also issued
+    per server. Default settings are hardcoded in bot's code.
+  * Welcomes and farewells => Bot is capable of setting a welcome and farewell messages system
+    per server. You can also set a DM message for user who joins or leaves your server.
+  * Minigames => Currently you can toss a dice or flip a coin. Dice is a standard 6-sided dice
+    and bot currently doesn't support other sides of dice (command parameters are ignored).
+    Coin is pretty much any money coin in any currency you can imagine (yes, even fictional).
+    However, the standard U.S. "quarter" coin (the US $0.25 coin) was on my mind, so the
+    default emojis that use unicode notations are approximations of what can be found on the
+    standard "quarter" => heads are represented by a royal head (princess, queen), as nothing
+    else I found in standard Discord emojis library, while tails are represented by a head of
+    an eagle typical for Northern America (white-head) => symbol of United States. Since this
+    coin is so widely known and used as base for refference of a coin used for coin flip,
+    I deciced to use that refference as well.
+  * Administration tools => basically most important feature of the bot. It will ban for you,
+    mute users for a specified time, all from a single input of a command line, which is the
+    chat text input window. You don't have to set up admin and moderator roles to use the
+    administration tools. The defaults are taken as commons from the permissions settings
+    typical for those roles (administrators having literally every permission and moderators
+    typically permitted to manage messages, reactions, assign roles to members, kick and ban).
+    However, if you have different settings for those roles (administrators may not have full
+    administration powers, rather owners may have) or you have roles that are channels
+    dependent, you'll want to set some of these roles.
+  * Experiences and leveling => every entertainment bot shall have this feature and this is
+    no exception! The formula for leveling taken from experiences may change in future.
+    Currently, no formula is used. I am, however, thinking of this one:
+    $$n = x^{l + 1} - x^l$$
+    $n$ is the required experience value to gain, $l$ is current level value, $x$ a further
+    unspecified constant (it's value is not yet determined). The cooldown for experience gain,
+    leveling and command fetch may be 15 seconds by default.
+  * Messaging guard => Bot guards the messages and searches up the strong words that a server
+    administrator can set up. They are actually regular expressions, following JavaScript `regex`
+    standards [found here](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions).
+    All of these regular expressions are joined together into a single one using switch operator
+    (`(word1|word2|word3|...)`), or if there is only one word, it is used directly. Bot then counts all
+    matches and deletes the message. If the matches count does not exceed maximum of 5 strong words, the
+    user is only warned for swearing. Maximum number of warnings per hour are 5, user is then muted for
+    an hour. If it exceeds the amount, user is straight muted for an hour serverwide. If you want to disable
+    messaging guard, just remove all regular expressions from server database, it will disable this feature.
+  * Memes and jokes => Dank memes, fun facts and other jokes are just waiting to be implemented.
+    You can of course expect memes lord search engine, fun facts API, and there will be also
+    a fake Google search engine provided. Well, it is basically just a normal Google search,
+    except not as you would expect. Since Google prohibits usage of Google API for automated
+    search worldwide (Google restricts API to custom search), and search engine usage is basically
+    the most basic skill on Internet, pure and priceless, Joker uses LMGTFY (which stands for
+    "Let me Google that for You") API instead, which performs a graphically nice animated Google
+    search and humiliates the person who asks for certain basic things that could be easily
+    searched up using (yes, you guessed it) Google.
+  * _Other features will be announced later_
+
+### Important notice
+Bot is an administrator and will attempt to send a welcome message for every server member
+as soon as it joins the server and informs about the `$$help` command there. If it fails
+to find the default channel (the central hub channel set in server settings), it tries
+to find the `#general` channel. If that fails, too, it tries to find the first channel
+it can contribute to, ordered from top to bottom. That will be literally the first channel
+it can find, so it is wise to have one channel named `#general` for this case.
--- main.js
+++ main.js
@@ -10,8 +10,10 @@
 require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client({disableEveryone: true});
-const mysql = require("mysql");
+const MySQL = require("mysql");
 const fs = require("fs");
+const Long = require("long");
+const DBL = require("dblapi.js");
 
 // Setup credentials
 client.credentials = {
@@ -21,9 +23,13 @@ client.credentials = {
         user: process.env.mysql_user,
         pswd: process.env.mysql_pswd,
         settings_db: process.env.mysql_db_prefix + "settings",
-        bans_mutes_db: process.env.mysql_db_prefix + "bans_mutes"
-    }
+        bans_mutes_db: process.env.mysql_db_prefix + "bans_mutes",
+        xp_levels_db: process.env.mysql_db_prefix + "xp_levels"
+    },
+    dbl_token: process.env.dbl_token
 };
+
+const dbl = new DBL(client.credentials.dbl_token, client);
 
 // Load default settings => deprecated, gonna be replaced by the settings in settings
 client.default_settings = Object.freeze({
@@ -67,24 +73,48 @@ client.settings = {
                 owner: Object.freeze([]),
                 admin: Object.freeze([]),
                 moderator: Object.freeze([]),
-                developer: Object.freeze([])
+                developer: Object.freeze([]),
+                muted: Object.freeze({
+                    general: null,
+                    channels: Object.freeze({})
+                })
             }),
             auditlog_channel: null,
             swearing_words: Object.freeze([]),
-            experiences: Object.freeze({})
+            welcomes: Object.freeze({
+                channel: "default",
+                message: "Welcome @user to @guild, have a great time!",
+                dm: null
+            }),
+            farewells: Object.freeze({
+                channel: "default",
+                message: "`@user` left our great community. Let's hope he'll come back one time.",
+                dm: null
+            })
         })
     },
     developers: {   // Developers of this bot
         chief: "370298001597399044",    // Chief developer (master) => fill in your own Discord id for your fork
-        additional: []                  // Additional developers list (fill in programatically from mysql database)
+        helping: []                  // Additional developers list (fill in programatically from mysql database)
     }
 };
 // Current settings => it needs to be filled by the mysql settings => settings per guild
 // Currently not used
 
-// Bans and mutes settings
-client.bans = {};       // Bans issued in every guild (fill in programatically)
-client.mutes = {};      // Mutes issued in every guild (fill in programatically)
+// Contact informations
+client.contact = {
+    email: "marpolda@gmail.com",        // Chief developer contact email
+    issue_tracker: "https://github.com/Polda18/Joker-Discord-Chat-Bot/issues",  // Link to issue tracker
+    discord_dm: client.fetchUser(client.settings.developers.chief).tag,         // Get the chief developer discord user tag
+    websites: "http://czghost.4fan.cz/contact/"          // Developer's sites
+};
+
+// Bans and mutes settings per guild
+client.bans = {};       // Bans issued in every guild (fill in using data fetched from MySQL database)
+client.mutes = {};      // Mutes issued in every guild (fill in using data fetched from MySQL database)
+
+// Users' experiences and levels per guild
+client.xp = {};         // Experiences and levels for each user per guild (fill in programatically)
 
 client.current_settings = {     // deprecated => gonna be deleted, refactored and replaced by mysql settings
     prefix: "$$",
@@ -109,7 +139,13 @@ client.current_settings = {     // deprecated => gonna be deleted, refactored an
 };
 
 // PERFORM A MYSQL CONNECTION == TODO
-// client.mysql_connection
+// client.mysql_connection = MySQL.createConnection({
+//     host: client.credentials.mysql.host,
+//     user: client.credentials.mysql.user,
+//     password: client.credentials.mysql.pswd
+// });
+
+// client.mysql_connection.connect();
 
 // Create a collection of bot commands
 client.commands_collection = new Discord.Collection();  // Save commands here
@@ -132,14 +168,117 @@ fs.readdir("./commands/", (err, files) => {
     console.log("Done...");
 });
 
+const getDefaultChannel = guild => {
+    // Get the system channel, if exists
+    if(guild.system_channel) return guild.system_channel;
+
+    // Get the original default channel, if exists
+    if(guild.channels.has(guild.id)) return guild.channels.get(guild.id);
+
+    // Get the default #general channel, if exists
+    let general = guild.channels.find(c => c.name === "general");
+    if(general) return general;
+
+    // Get the first writeable channel it finds
+    // Beware that this will find any first writeable channel, which may be any if the bot has admin privilieges!
+    // It is wise to have one channel named #general in your server for this case. This is really the last hope
+    // to thank you for adding it in your server. Since this bot is administrator, the first channel it can write to
+    // is literally the first channel it can find.
+    return guild.channels.filter(c => c.type === "text" && c.permissionsFor(guild.client.user).has("SEND_MESSAGES"))
+        .sort((a, b) => a.position - b.position || Long.fromString(a.id).sub(Long.fromString(b.id)).toNumber())
+        .first();;
+}
+
 // This will be run when the bot is ready to roll out
 client.on('ready', () => {
-    console.log(`Logged in as "${client.user.tag}" with id "${client.user.id}".`);  // Says when the bot is ready
+    console.log(`Logged in as \`${client.user.tag}\` with id \`${client.user.id}\``);  // Says when the bot is ready
 });
+
+// Joined a guild? Roll out:
+client.on('guildCreate', guild => {
+    console.log(`Joined a guild \`${guild.name}\` with id \`${guild.id}\``);
+
+    let join_message = `Hey! Thanks for adding me in! Use \`${client.settings.guilds.default.prefix}help\` to get info on how to use my functions I provide and \`${client.settings.guilds.default.prefix}help settings\` on how to properly save my server dependent settings.`;
+
+    // Get the default channel, usually noted as #general
+    let system_channel = getDefaultChannel(guild);
+    if(system_channel) await system_channel.send(join_message);
+
+    // Update the settings by adding a new guild to the mysql database
+    // TODO: perform a mysql query to update settings in a mysql database
+
+    // TODO: perform a mysql query to update JSON settings from a mysql database
+});
+
+// Kicked from a guild? Back up:
+client.on('guildDelete', guild => {
+    console.log(`Kicked from a guild \`${guild.name}\` with id \`${guild.id}\``);
+
+    let leave_message = {
+        intro: `I'm sorry that I have to leave. Did I do something wrong? Please`,
+        please: `contact my chief developer`,
+        contacts: [],
+        outro: `I'm looking forward to see you again`
+    };
+    if(client.contact.discord_dm) leave_message.contacts.push(`directly on Discord DM to \`${client.contact.discord_dm}\``);
+    if(client.contact.email) leave_message.contacts.push(`via e-mail to ${client.contact.email}`);
+    if(client.contact.websites) leave_message.contacts.push(`using websites at ${client.contact.websites}`);
+    if(client.contact.issue_tracker) leave_message.contacts.push(`send a bug report or another issue to issue tracker at ${client.contact.issue_tracker}`);
+
+    let message = leave_message.intro;
+    for(i = 0; i < leave_message.contacts.length; ++i) {
+        switch(i) {
+            case 0:
+                if(
+                    leave_message.contacts[i].contains(client.contact.discord_dm)
+                    || leave_message.contacts[i].contains(client.contact.email)
+                    || leave_message.contacts[i].contains(client.contact.websites)
+                ) message += ` ${leave_message.please}`;
+                message += ` ${leave_message.contacts[i]}`;
+                break;
+            case leave_message.contacts.length - 1:
+                message += `or ${leave_message.contacts[i]}`;
+                break;
+            default:
+                message += `, ${leave_message.contacts[i]}`;
+        }
+    }
+    message += `. ${leave_message.outro}`;
+
+    return await getDefaultChannel(guild.id).send(message);
+});
+
+// Guild changed name? Announce it:
+client.on('guildUpdate', (oGuild, nGuild) => {
+    if(oGuild.id !== nGuild.id) return;     // Ignore cross guild change (maybe not gonna happen, but better safe than sorry)
+    if(oGuild.name !== nGuild.name) {
+        console.log(`Guild with id ${oGuild.id} changed name from \`${oGuild.name}\` to \`${nGuild.name}\``);
+    }
+});
+
+// Member joined? Welcome him!
+client.on('guildMemberAdd', member => {
+    console.log(`A user \`${member.user.tag}\` with id \`${member.user.id}\` joined guild \`${member.guild.name}\` with id \`${member.guild.id}\``);
+
+    // TODO: Perform a MySQL query and fetch set welcome message for current guild (or retrieve default)
+});
+
+// Member left? Farewell him!
+client.on('guildMemberRemove', member => {
+    console.log(`A user \`${member.user.tag}\` with id \`${member.user.id}\` left guild \`${member.guild.name}\` with id \`${member.guild.id}\``);
+
+    // TODO: Perform a MySQL query and fetch set farewell message for current guild (or retrieve default)
+});
+
+// Member received new role? Praise him!
+client.on('guildMemberUpdate', member => {
+    console.log(`A user \`${member.user.tag}\` with id \`${member.user.id}\` has been updated`);
+})
 
 // Scan a message for triggers
 client.on('message', message => {
-    if (message.channel.type === "dm" || message.channel.type === "group") return; // ignore direct messages or group dm messages
+    if (message.channel.type === "dm" || message.channel.type === "group") return; // Ignore direct messages or group dm messages
+    if (message.author.bot) return;     // Ignore messages by bots
     
     let prefix = client.current_settings.prefix;
     let msgArray = message.content.split(" ");
@@ -155,6 +294,6 @@ client.on('message', message => {
 
         // TODO: Experiences
     }
-})
+});
 
 client.login(client.credentials.token);     // Login the bot to the Discord service
